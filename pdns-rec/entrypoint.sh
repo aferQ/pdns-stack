6539104e15ca32b030d8dcb3cb5a81605a540549
aferQ <maksymilian.kozicz@outlook.com>
2025-10-11 01:51:01 +0200

    Some clean up and added config as volume to auth server

--- pdns-rec/entrypoint.sh
+++ pdns-rec/entrypoint.sh
@@ -3,13 +3,9 @@
 # Exit immediately if a command exits with a non-zero status to prevent unexpected behavior
 set -e
 
-# Optional: print some info
-echo "Starting PowerDNS Recursor..."
-
 # Making socket dir
 mkdir -p /var/run/pdns-recursor
 chown -R $(whoami) /var/run/pdns-recursor
-
 
 # Start pdns_recursor in foreground with proper options for Docker
 pdns_recursor --config-dir=/etc/powerdns/ --daemon=no
